1e27ce07716e9e362932fbe0d77b6ddced2dd679
unknown <arunsdass23@gmail.com>
2018-05-17 22:11:41 -0700

    retooled the style

--- pctime/views.py
+++ pctime/views.py
@@ -3,21 +3,17 @@ import requests
 
 def movies(request):
 
-    response = requests.get('https://yts.am/api/v2/list_movies.json?sort_by=date_added&sort_by=year&limit=20&minimum_rating=7&order_by=desc')
+    response = requests.get('https://yts.am/api/v2/list_movies.json?sort_by=date_added&sort_by=year&limit=20&minimum_rating=7&order_by=desc&limit=21')
     movies = response.json()
     titles=[]
-    ids=[]
+    movie_covers=[]
+    summary=[]
     for k,element in enumerate(movies['data']['movies']):
         titles.append(element['title'])
-        ids.append(element['id'])
-    img_ids='&'.join(str(v) for v in ids)
-    request_string= 'https://yts.am/api/v2/movie_details.json?movie_id='+ img_ids + '&with_images=true'
-    image_response=requests.get(request_string)
-    img = image_response.json()
-    movie_covers=[]
-    for k,element in enumerate(movies['data']['movies']):
         movie_covers.append(element['medium_cover_image'])
-    movie_info=zip(titles,movie_covers)
+        summary.append(element['summary'])
+
+    movie_info=zip(titles,movie_covers,summary)
     return render(request, 'pctime.html',{
     'movie_info':movie_info,
     })
